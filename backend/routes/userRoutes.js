1d554a927bd2a1efb66c511f3459968c1ab289b0
cyrus311 <shadow15mr@hotmail.com>
2022-04-01 23:14:48 +0300

    user api added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  getMe
+} = require("../controllers/userController");
 
 router.route("/").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/me").get(getMe);
+
+module.exports = router;
