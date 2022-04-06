af6205f588ad2d01be1af2b32d0dbaf80bd4765d
cyrus311 <shadow15mr@hotmail.com>
2022-04-06 17:51:28 +0300

    authentication added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,9 +5,10 @@ const {
   loginUser,
   getMe
 } = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/me").get(getMe);
+router.get("/me", protect, getMe);
 
 module.exports = router;
